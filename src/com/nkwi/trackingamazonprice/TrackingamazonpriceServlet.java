911984085b3565cf783d77156c5d738fd59610c8
LinkB3AT <linkb3at@gmail.com>
2016-11-07 03:04:07 +0100

    Google App Engine Projekt erstellt

---
+++ src/com/nkwi/trackingamazonprice/TrackingamazonpriceServlet.java
@@ -0,0 +1,12 @@
+package com.nkwi.trackingamazonprice;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class TrackingamazonpriceServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
